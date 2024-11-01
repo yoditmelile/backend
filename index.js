c25e5d1247247aeb87e62334440134c8b2388ecd
Yodit Melile <96232166+yoditmelile@users.noreply.github.com>
2024-11-01 15:39:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const contactRouter = require('./contact'); 
-const affiliateRouter = require('./Register'); 
+const affiliateRouter = require('./Register');  
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/", contactRouter);
-app.use("/", affiliateRouter); 
+
+app.use("/contact", contactRouter);  
+app.use("/Register", affiliateRouter); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
